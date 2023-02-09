1f4787e7b89469ea683523b9d8be3d33df8a0c75
Guilherme <guilhermeshadow91@gmail.com>
2023-02-09 16:33:55 -0300

    feat: using the middleware in userModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { VerifyIdMiddleware } from '../middlewares/userId.middleware';
 
 @Module({
   imports: [],
   controllers: [UserController],
   providers: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(VerifyIdMiddleware).forRoutes({ path: 'user/:id', method: RequestMethod.GET}, {path: "user/:id", method: RequestMethod.PUT});
+  }
+}
