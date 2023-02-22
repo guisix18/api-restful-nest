93a3d9d53b60d8eb5c448a634026ed37ada11f61
Guilherme <guilhermeshadow91@gmail.com>
2023-02-22 17:13:10 -0300

    refactor: removing the entity user

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,35 +1,28 @@
-import { IsEmail, IsNotEmpty, MinLength, IsOptional } from "class-validator";
+import { IsEmail, IsNotEmpty, MinLength, IsOptional } from 'class-validator';
 
 export class UserBodyDto {
-    @IsNotEmpty({ message: "The name field can't be empty." })
-    name: string;
+  @IsNotEmpty({ message: "The name field can't be empty." })
+  name: string;
 
-    @IsEmail(undefined, { message: 'Email is not valid' })
-    email: string;
+  @IsEmail(undefined, { message: 'Email is not valid' })
+  email: string;
 
-    @IsNotEmpty()
-    @MinLength(8, { message: 'The password has to be greater or equal than 8' })
-    password: string
+  @IsNotEmpty()
+  @MinLength(8, { message: 'The password has to be greater or equal than 8' })
+  password: string;
 }
 
 export class UserBodyUpdateDto {
-    @IsOptional()
-    @IsNotEmpty()
-    name: string;
+  @IsOptional()
+  @IsNotEmpty()
+  name: string;
 
-    @IsOptional()
-    @IsEmail(undefined, { message: 'Email is not valid' })
-    email: string;
+  @IsOptional()
+  @IsEmail(undefined, { message: 'Email is not valid' })
+  email: string;
 
-    @IsOptional()
-    @IsNotEmpty()
-    @MinLength(8, { message: 'The password has to be greater or equal than 8'})
-    password: string;
-}
-
-export class UserReturn {
-    id: string;
-    name: string;
-    email: string;
-    password?: string;
+  @IsOptional()
+  @IsNotEmpty()
+  @MinLength(8, { message: 'The password has to be greater or equal than 8' })
+  password: string;
 }
