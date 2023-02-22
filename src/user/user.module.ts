ba85bf60097ff07d52a60589f33d5154bdc8cd3d
Guilherme <guilhermeshadow91@gmail.com>
2023-02-22 17:14:58 -0300

    feat: import prismaModule in userModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,17 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { VerifyIdMiddleware } from '../middlewares/userId.middleware';
 import { VerifyUserEmailMiddleware } from 'src/middlewares/verifyUserEmail.middleware';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [UserController],
   providers: [UserService],
 })
@@ -13,9 +19,11 @@ export class UserModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(VerifyIdMiddleware)
-      .forRoutes({ path: 'user/:id', method: RequestMethod.GET },
-        { path: "user/:id", method: RequestMethod.PUT },
-        { path: "user/:id", method: RequestMethod.DELETE });
+      .forRoutes(
+        { path: 'user/:id', method: RequestMethod.GET },
+        { path: 'user/:id', method: RequestMethod.PUT },
+        { path: 'user/:id', method: RequestMethod.DELETE },
+      );
 
     consumer
       .apply(VerifyUserEmailMiddleware)
