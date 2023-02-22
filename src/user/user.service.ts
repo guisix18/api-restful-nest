1fb720b0bc705bac8bcada79d0d2398faeaddc4a
Guilherme <guilhermeshadow91@gmail.com>
2023-02-22 17:15:29 -0300

    refactor: a new form to create a user using type by prisma

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,57 +1,83 @@
-import { Injectable } from "@nestjs/common";
-import { UserBodyDto, UserBodyUpdateDto, UserReturn } from "./dto/user.dto";
-import { prisma } from "prisma/client/client";
-import { randomUUID } from "crypto";
+import { Injectable } from '@nestjs/common';
+import { UserBodyDto, UserBodyUpdateDto } from './dto/user.dto';
+import { User } from './entities/user.entities';
+import { randomUUID } from 'crypto';
+import * as bcrypt from 'bcrypt';
+import { UserList } from './entities/user.entities';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UserService {
+    constructor(private readonly prisma: PrismaService) { }
 
-    async create(dto: UserBodyDto): Promise<UserReturn> {
-        const createUser = await prisma.user.create({
-            data: {
-                id: randomUUID(),
-                name: dto.name,
-                email: dto.email,
-                password: dto.password
-            }, select: { id: true, name: true, email: true }
-        })
+    async create(dto: UserBodyDto): Promise<User> {
+        const data: Prisma.UserCreateInput = {
+            id: randomUUID(),
+            ...dto,
+            password: await bcrypt.hash(dto.password, 8)
+        }
 
-        return createUser;
+        const createUser = await this.prisma.user.create({ data });
+
+        return {
+            ...createUser,
+            password: undefined
+        };
     }
 
-    async list(): Promise<UserReturn[]> {
-        return await prisma.user.findMany({ select: { id: true, name: true, email: true } })
+    async list(): Promise<UserList[]> {
+        return await this.prisma.user.findMany({
+            select: {
+                id: true,
+                name: true,
+                email: true
+            },
+        });
     }
 
-    async listOne(id: string): Promise<UserReturn> {
-        return await prisma.user.findFirst({ where: { id }, select: { id: true, name: true, email: true } });
+    async listOne(id: string): Promise<Partial<User>> {
+        return await this.prisma.user.findFirst({
+            where: { id },
+            select: {
+                id: true,
+                name: true,
+                email: true
+            },
+        });
     }
 
-    async update(id: string, body: UserBodyUpdateDto): Promise<UserReturn> {
-        const findUser = await prisma.user.findFirst({ where: { id } });
+    async update(id: string, body: UserBodyUpdateDto): Promise<Partial<User>> {
+        const findUser = await this.prisma.user.findFirst({
+            where: {
+                id
+            },
+        });
 
-        const userUpdated = await prisma.user.update({
+        const userUpdated = await this.prisma.user.update({
             where: {
                 id,
             },
             data: {
                 name: body.name ? body.name : findUser.name,
                 email: body.email ? body.email : findUser.email,
-                password: body.password ? body.password : findUser.password
+                password: body.password ? body.password : findUser.password,
             },
             select: {
                 id: true,
                 name: true,
-                email: true
-            }
-        })
+                email: true,
+            },
+        });
         return userUpdated;
     }
 
     async delete(id: string): Promise<void> {
-        await prisma.user.delete({
-            where: { id }
-        })
+        await this.prisma.user.delete({
+            where: {
+                id
+            },
+        });
         return;
     }
 }
