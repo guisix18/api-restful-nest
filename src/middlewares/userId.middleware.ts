ea534b6c68cccf8cafead0bf131cf699be663980
Guilherme <guilhermeshadow91@gmail.com>
2023-02-22 17:12:16 -0300

    refactor: prisma in constructor using the prismaService

--- src/middlewares/userId.middleware.ts
+++ src/middlewares/userId.middleware.ts
@@ -1,20 +1,21 @@
-import { Injectable, NestMiddleware } from "@nestjs/common";
-import { Request, Response, NextFunction } from "express";
-import { prisma } from "prisma/client/client";
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class VerifyIdMiddleware implements NestMiddleware {
-    async use(req: Request, res: Response, next: NextFunction) {
-        const { id } = req.params;
 
-        const findUser = await prisma.user.findFirst({where: {id}});
+  constructor(private readonly prisma: PrismaService) {}
 
+  async use(req: Request, res: Response, next: NextFunction) {
+    const { id } = req.params;
 
-        if(!findUser) {
-            return res.status(400).json({ message: "User not found" })
-        }
+    const findUser = await this.prisma.user.findFirst({ where: { id } });
 
-        next();
-
+    if (!findUser) {
+      return res.status(400).json({ message: 'User not found' });
     }
+
+    next();
+  }
 }
