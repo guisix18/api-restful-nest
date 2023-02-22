8362de33476fa85bfa21f294eca99f5393247199
Guilherme <guilhermeshadow91@gmail.com>
2023-02-22 17:14:22 -0300

    refactor: formated code

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,45 +1,56 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Res } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
-import { response, Response } from "express";
-import { UserBodyDto } from "./dto/user.dto";
-import { UserBodyUpdateDto } from "./dto/user.dto";
-import { UserService } from "./user.service";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Res
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
+import { UserBodyDto, UserBodyUpdateDto } from './dto/user.dto';
+import { UserService } from './user.service';
 
 @ApiTags('User')
 @Controller('/user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
 
-    constructor(private readonly userService: UserService) {}
+  @Post()
+  async createUser(@Res() response: Response, @Body() body: UserBodyDto) {
+    const user = await this.userService.create(body);
+    return response.status(201).send(user);
+  }
 
-    @Post()
-    async createUser(@Res() response: Response, @Body() body: UserBodyDto) {
-        const user = await this.userService.create(body)
-        return response.status(201).send(user);
-    }
+  @Get()
+  async getUser(@Res() response: Response) {
+    const users = await this.userService.list();
+    return response.status(200).send(users);
+  }
 
-    @Get()
-    async getUser(@Res() response: Response) {
-        const users = await this.userService.list();
-        return response.status(200).send(users);
-    }
+  @Get('/:id')
+  async getOneUser(@Res() response: Response, @Param('id') id: string) {
+    const oneUser = await this.userService.listOne(id);
+    return response.status(200).send(oneUser);
+  }
 
-    @Get('/:id')
-    async getOneUser(@Res() response: Response, @Param('id') id: string) {
-        const oneUser = await this.userService.listOne(id);
-        return response.status(200).send(oneUser);
-    }
+  @Put('/:id')
+  async updateUser(
+    @Res() response: Response,
+    @Param('id') id: string,
+    @Body() body: UserBodyUpdateDto,
+  ) {
+    const updatedUser = await this.userService.update(id, body);
+    return response.status(200).send(updatedUser);
+  }
 
-    @Put('/:id')
-    async updateUser(@Res() response: Response, @Param('id') id: string, @Body() body: UserBodyUpdateDto) {
-        const updatedUser = await this.userService.update(id, body);
-        return response.status(200).send(updatedUser)
-    }
-
-    @Delete('/:id')
-    async deleteUser(@Res() response: Response, @Param('id') id: string) {
-        await this.userService.delete(id);
-        return response.status(200).send({
-            message: "User deleted"
-        })
-    }
+  @Delete('/:id')
+  async deleteUser(@Res() response: Response, @Param('id') id: string) {
+    await this.userService.delete(id);
+    return response.status(200).send({
+      message: 'User deleted',
+    });
+  }
 }
