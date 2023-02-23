fbb1bd227177decee4c40f6f2f7390d045a3d8c4
Guilherme <guilhermeshadow91@gmail.com>
2023-02-23 16:05:56 -0300

    feat(auth): providers in appModule using JwtAuthGuard

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [PrismaModule, UserModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [{
+    provide: APP_GUARD,
+    useClass: JwtAuthGuard
+  }],
 })
-export class AppModule {}
+export class AppModule { }
