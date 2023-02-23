ca9dd91164ece97a42eb9f58858e613ce0f6a946
Guilherme <guilhermeshadow91@gmail.com>
2023-02-23 15:36:00 -0300

    feat(user): export user service to be used in auth

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -14,6 +14,7 @@ import { PrismaModule } from 'src/prisma/prisma.module';
   imports: [PrismaModule],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
