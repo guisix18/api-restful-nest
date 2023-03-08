b8fca7a5dc5da65ba5c79fc053fb8bc68209488d
Guilherme <guilhermeshadow91@gmail.com>
2023-03-08 16:29:07 -0300

    feat(app): import ProductsModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [PrismaModule, UserModule, ProductsModule, AuthModule],
   controllers: [],
   providers: [{
     provide: APP_GUARD,
