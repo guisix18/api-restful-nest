000e64fe418b9b2c69d02dd315cecd34d8e99fdf
Guilherme <guilhermeshadow91@gmail.com>
2023-03-08 16:37:38 -0300

    feat(products): Method GET in products controller to GET all products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post, Res } from "@nestjs/common";
+import { Body, Controller, Post, Get, Res } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { ProductBodyDto } from "./dto/products.dto";
 import { ProductsService } from "./products.service";
 import { Response } from "express";
+import { IsPublic } from "src/auth/decorators/is-public.decorator";
 
 @ApiTags('Products')
 @Controller('/products')
@@ -13,5 +14,12 @@ export class ProductsController {
     async createProduct(@Res() response: Response, @Body() body: ProductBodyDto) {
         const product = await this.productsService.create(body);
         return response.status(201).send(product);
+    }
+
+    @IsPublic()
+    @Get()
+    async listProducts(@Res() respose: Response) {
+        const products = await this.productsService.list();
+        return respose.status(200).send(products);
     }
 }
