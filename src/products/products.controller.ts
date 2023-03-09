186276bfe1a38be0ba9b306a501c203e1ec6d1aa
Guilherme <guilhermeshadow91@gmail.com>
2023-03-09 17:08:43 -0300

    feat(products): Get one product by id in products controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, Post, Get, Res } from "@nestjs/common";
+import { Body, Controller, Post, Get, Res, Param } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { ProductBodyDto } from "./dto/products.dto";
 import { ProductsService } from "./products.service";
-import { Response } from "express";
+import { response, Response } from "express";
 import { IsPublic } from "src/auth/decorators/is-public.decorator";
 
 @ApiTags('Products')
@@ -21,5 +21,12 @@ export class ProductsController {
     async listProducts(@Res() respose: Response) {
         const products = await this.productsService.list();
         return respose.status(200).send(products);
+    }
+
+    @IsPublic()
+    @Get('/:id')
+    async listOneProduct(@Res() response: Response, @Param('id') id: string) {
+        const oneProduct = await this.productsService.listOne(id);
+        return response.status(200).send(oneProduct)
     }
 }
