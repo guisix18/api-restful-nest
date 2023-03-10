cd0ff6f9c049133f0143993bf796c243850d4fe5
Guilherme <guilhermeshadow91@gmail.com>
2023-03-10 16:50:25 -0300

    feat(products): DTO Body to update a Product

--- src/products/dto/products.dto.ts
+++ src/products/dto/products.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumber, MinLength } from "class-validator";
+import { IsNotEmpty, IsNumber, IsOptional, MinLength } from "class-validator";
 
 
 export class ProductBodyDto {
@@ -8,6 +8,21 @@ export class ProductBodyDto {
     @IsNumber()
     price: number;
 
+    @IsNotEmpty({ message: "Please insert a description" })
+    @MinLength(10, {message: "The description has to be greater or equal than 10"})
+    description: string;
+}
+
+export class ProductBodyUpdateDto {
+    @IsOptional()
+    @IsNotEmpty({ message: "The product name field can't be empty." })
+    product_name: string;
+
+    @IsOptional()
+    @IsNumber()
+    price: number;
+
+    @IsOptional()
     @IsNotEmpty({ message: "Please insert a description" })
     @MinLength(10, {message: "The description has to be greater or equal than 10"})
     description: string;
