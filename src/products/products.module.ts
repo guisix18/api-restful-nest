1f30e079f6cbaf3f7d1a20e78fb733228431d6b4
Guilherme <guilhermeshadow91@gmail.com>
2023-03-29 16:04:21 -0300

    refactor(product): Products module using middlewares

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,4 +1,5 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
+import { verifyIdProductMiddleware } from "src/middlewares/Product/productId.middleware";
 import { PrismaModule } from "src/prisma/prisma.module";
 import { ProductsController } from "./products.controller";
 import { ProductsService } from "./products.service";
@@ -9,4 +10,14 @@ import { ProductsService } from "./products.service";
     providers: [ProductsService],
     exports: [ProductsService],
 })
-export class ProductsModule { }
+export class ProductsModule implements NestModule { 
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(verifyIdProductMiddleware)
+            .forRoutes(
+                { path: 'products/:id', method: RequestMethod.GET },
+                { path: 'products/:id', method: RequestMethod.PUT },
+                { path: 'products/:id', method: RequestMethod.DELETE },
+            )
+    }
+}
