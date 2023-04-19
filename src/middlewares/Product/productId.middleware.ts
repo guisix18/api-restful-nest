5695338fbacbe11fede385f9055beceedf39207b
Guilherme <guilhermeshadow91@gmail.com>
2023-04-19 11:32:43 -0300

    refactor(products): Using the util in json response

--- src/middlewares/Product/productId.middleware.ts
+++ src/middlewares/Product/productId.middleware.ts
@@ -1,7 +1,7 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
 import { PrismaService } from "src/prisma/prisma.service";
-
+import { ERROR_PRODUCT_ID_NOT_FOUND } from "src/utils/Product/products.utils";
 
 @Injectable()
 export class verifyIdProductMiddleware implements NestMiddleware {
@@ -13,7 +13,7 @@ export class verifyIdProductMiddleware implements NestMiddleware {
         const findProduct = await this.prisma.products.findFirst({ where: { id } });
 
         if (!findProduct) {
-            return res.status(400).json({ message: "Product not found" });
+            return res.status(400).json({ ERROR_PRODUCT_ID_NOT_FOUND });
         }
         next();
     }
