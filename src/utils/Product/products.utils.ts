c9e135b1fe723f787e22118c7ac47f35d9dfac94
Guilherme <guilhermeshadow91@gmail.com>
2023-04-19 11:32:11 -0300

    feat(utils): Utils file to clean repeated code

---
+++ src/utils/Product/products.utils.ts
@@ -0,0 +1,2 @@
+export const ERROR_PRODUCT_ID_NOT_FOUND = { message: "Product not found" };
+export const DELETE_PRODUCT_CONTROLLER = { message: "Product deleted" };
---
+++ src/utils/User/user.utils.ts
@@ -0,0 +1,3 @@
+export const ERROR_EMAIL_MIDDLEWARE = { message: 'This email is already being used' };
+export const ERROR_USER_ID_NOT_FOUND_MIDDLEWARE = { message: 'User not found' };
+export const DELETE_USER_CONTROLLER = { message: 'User deleted' };
